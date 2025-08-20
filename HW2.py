2a5e421ca5b099d4b7eaf811cd19a0d6fc3081ab
Brodie Yocham <by127394@student.wpsok.org>
2025-08-20 13:47:31 -0500

    Added HW 1 Added HW 2

---
+++ HW2.py
@@ -0,0 +1,29 @@
+#Name: Brodie Yocham
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#1.Print: ("Hello World")
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+oneWord = "Hello World"
+A_Number = 42
+truefalse = False
+#3. Print all three variables on the same print function (at the same time).
+print("Luffy")
+#4. Create a variable that asks the user to input an integer.
+sword = int(input("swing sword "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+number = A_Number + sword
+print(number)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(number / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+truefalse = True
+#8. Print the value of the boolean variable.
+print(truefalse)
+#9. Create a variable with a number that contains decimals.
+decimal = 5.5
+#10. Round the number from #9 up or down using the round function.
+print(round(decimal))
