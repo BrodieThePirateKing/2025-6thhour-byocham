3723e505b0e960ff5b1f21c84b4c41b097709a96
Brodie Yocham <by127394@student.wpsok.org>
2025-09-23 13:29:52 -0500

    Added playground

---
+++ playground.py
@@ -0,0 +1,7 @@
+#rps
+import random
+
+hand_rps = ["you live", "your family and friends die in a fiery explosion caused by a gas leak in your home"]
+print(hand_rps)
+random.shuffle(hand_rps)
+print(hand_rps)
